14fe5931324acb01a8c70914ac11ef4823d34e4a
AwesomeNinja886 <awesomeninja886@gmail.com>
2023-04-28 21:43:23 -0400

    lang files + item rarity

--- src/main/java/com/awesomeninja/elemental/item/ModItems.java
+++ src/main/java/com/awesomeninja/elemental/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,8 +16,8 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Elemental.MODID);
 
-    public static final RegistryObject<Item> CRYOTHEUM_DUST = ITEMS.register("cryotheum_dust", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-    public static final RegistryObject<Item> GELID_CRYOTHEUM_BUCKET = ITEMS.register("gelid_cryotheum_bucket", () -> new BucketItem(ModFluids.SOURCE_GELID_CRYOTHEUM, new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS).stacksTo(1).craftRemainder(Items.BUCKET)));
+    public static final RegistryObject<Item> CRYOTHEUM_DUST = ITEMS.register("cryotheum_dust", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS).rarity(Rarity.EPIC)));
+    public static final RegistryObject<Item> GELID_CRYOTHEUM_BUCKET = ITEMS.register("gelid_cryotheum_bucket", () -> new BucketItem(ModFluids.SOURCE_GELID_CRYOTHEUM, new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS).stacksTo(1).craftRemainder(Items.BUCKET).rarity(Rarity.EPIC)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
